e8cb7502220fc2b3ebd69c4edb4ca74cce963708
leio zhang <leio zhang>
2016-10-08 11:27:00 +0800

    project added

---
+++ services/ModelVoxelizerService.lua
@@ -0,0 +1,14 @@
+--[[
+Title: ModelVoxelizer Service 
+Author(s): leio
+Date: 2016/10/8
+Desc: 
+use the lib:
+------------------------------------------------------------
+NPL.load("(gl)npl_packages/ModelVoxelizer/services/ModelVoxelizerService.lua");
+local ModelVoxelizerService = commonlib.gettable("ModelVoxelizer.services.ModelVoxelizerService");
+------------------------------------------------------------
+]]
+local ModelVoxelizerService = commonlib.gettable("ModelVoxelizer.services.ModelVoxelizerService");
+function ModelVoxelizerService.main()
+end
