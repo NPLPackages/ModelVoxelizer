1a2d786021b9a6fb9ee98dd4f29ece50a9fe821a
leio zhang <leio zhang>
2016-10-08 12:40:41 +0800

    changed namespace

---
+++ Mod/ModelVoxelizer/services/ModelVoxelizerService.lua
@@ -0,0 +1,14 @@
+--[[
+Title: ModelVoxelizer Service 
+Author(s): leio
+Date: 2016/10/8
+Desc: 
+use the lib:
+------------------------------------------------------------
+NPL.load("(gl)Mod/ModelVoxelizer/services/ModelVoxelizerService.lua");
+local ModelVoxelizerService = commonlib.gettable("Mod.ModelVoxelizer.services.ModelVoxelizerService");
+------------------------------------------------------------
+]]
+local ModelVoxelizerService = commonlib.gettable("Mod.ModelVoxelizer.services.ModelVoxelizerService");
+function ModelVoxelizerService.main()
+end
